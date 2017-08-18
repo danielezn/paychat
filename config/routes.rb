b9f6b9bf35f6e723d3ef11cd31e31c6644597063
Daniel Zarate <danielezn@hotmail.com>
2017-08-18 00:10:20 -0300

    define structure project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,39 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
   get '*path', to: 'dashboard#index'
+  namespace :api, :defaults => {:format => :json}, :path => "", :constraints => {:subdomain => "api"} do
+    namespace :v1 do
+      # Usuario
+      ## Autenticacion
+      ## Crear Usuario
+      ## Editar Usuario
+      ## Obtener Usuario
+
+      # Medios de Pagos
+      ## Agregar medios de pagos
+      ## Editar medios de pagos
+      ## Eliminar medios de pagos
+      ## Obtener medios de pagos
+
+
+      # Conferencias
+      ## Crear conferencia
+      ## Editar conferencia
+      ## Cancelar conferencia
+      ## Obtener conferencias
+
+      # Pagos
+      ## Hacer pago
+
+      # Actividades
+      ## Registrar actividades
+      ## Obtener actividades
+    end
+  end
 end
+
+# class API::PeopleController < ApplicationController
+# end
+
+# Token authentication
+# http://www.mccartie.com/2016/11/03/token-based-api-authentication.html
