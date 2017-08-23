a13056910fa68b835c20750a40c72a99b93d4cf5
Daniel Zarate <danielezn@hotmail.com>
2017-08-22 21:47:27 -0300

    token authentication implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def generate_auth_token
+    token = SecureRandom.hex
+    self.update_columns(auth_token: token, token_created_at: Time.zone.now)
+    token
+  end
+
+  def invalidate_auth_token
+    self.update_columns(auth_token: nil, token_created_at: nil)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822001026) do
+ActiveRecord::Schema.define(version: 20170823001456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,9 @@ ActiveRecord::Schema.define(version: 20170822001026) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token"
+    t.datetime "token_created_at"
+    t.index ["auth_token", "token_created_at"], name: "index_users_on_auth_token_and_token_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
