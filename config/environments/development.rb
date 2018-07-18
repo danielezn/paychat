c1bc90e322177cc24520633918bd79f18e102bc1
Daniel Zarate <danielezn@hotmail.com>
2018-07-17 21:47:46 -0400

    remove assets configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,14 +37,6 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
-
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
-
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations
